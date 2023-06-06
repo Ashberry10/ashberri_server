b51dd3f546ece5ccb3144c09cf730e8a536a26d0
Aman kumar <amankumar02740@gmail.com>
2023-06-06 22:33:51 +0530

    again testing .....................................................

--- friend/models.py
+++ friend/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.conf import settings
 
 
-# Create your models here...
+# Create your models here.....
 class FriendList(models.Model):
 
     user =  models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
