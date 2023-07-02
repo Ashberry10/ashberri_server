b2c4a3e07f1d8f5ec32aae42ea2913513b4f11ee
Ashish Thakur <50015357+AshishThakur10@users.noreply.github.com>
2023-07-02 22:39:35 +0530

    post upload

--- account/urls.py
+++ account/urls.py
@@ -1,39 +1,16 @@
-
-# from account.views import UserPasswordResetView,UserRegistrationView,SendPasswordResetEmailView,UserLoginView,AllStudents,UserProfileView,UserChangePassword
-# from account.modelapiview import ModelapiView, FriendStatusAndCompatibilityAPIView,FriendStatusAndCompatibilitByIdAPIView
-from account.views import UserLoginView,UserRegistrationView,AllUser,UserProfileView,UpdateUser,UserDeleteView
-from account.modelapiview import  FriendStatusAndCompatibility,FriendStatusAndCompatibilityById
-
-from  account import views
-from django.urls import path,include
+from account.modelapiview import FriendStatusAndCompatibility, FriendStatusAndCompatibilityById
+from account.views import AllUser, UpdateUser, UserDeleteView, UserLoginView, UserProfileView,UserRegistrationView, uploadPost
+from django.urls import path
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(),name='register'),
-    # path('register/<int:id>/profile/', UserRegistrationView.as_view(),name='register'),
-
     path('login/', UserLoginView.as_view(),name='login'),
+    path('post/upload/',uploadPost.as_view(), name='upload'),
     path('getallusers/', AllUser.as_view(),name='getallusers'),
     path('getallusers/<int:id>',AllUser.as_view()),
     path('update/',UpdateUser.as_view(), name='user_update'),
-    # path('getalluserswithcomp/', AllUserwithComp.as_view(),name='getalluserswithcomp'),
-    # path('profile/<user_id>', UserProfileView.as_view(), name='profile'),
     path('profile/', UserProfileView.as_view(), name='profile'),
-    # path('Modelapi/',ModelapiView.as_view(), name='ModelapiView'),
-    # path('Modelapi/<int:id>',ModelapiView.as_view(),name='ModelapiView'),
     path('friendStatusAndCompatibility/', FriendStatusAndCompatibility.as_view(), name='friend_status_and_compatibility'),
-    # path('friendStatusAndCompatibilitById/<int:id>', FriendStatusAndCompatibilityById.as_view(), name='friendStatusAndCompatibilitById'),
     path('friendStatusAndCompatibilityById/', FriendStatusAndCompatibilityById.as_view(), name='friendStatusAndCompatibilitById'),
-
-
     path('deleteuser/<int:id>', UserDeleteView.as_view(), name='user-delete'),
-    # path('course/<int:courseid>' ,views.courseDetails),
-
-    # path('course/<int:courseid>/<int:secondid>' ,views.courseDetails)
-
-
-
-   
-    # path('changepassword/', UserChangePassword.as_view(), name='changepassword'),
-    # path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
-      # path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),
 ] 
--- account/views.py
+++ account/views.py
@@ -1,39 +1,21 @@
+from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.parsers import FormParser,MultiPartParser,JSONParser
-from account.serializers import UpdateUserSeriailzer, UserRegistrationSerializer,GetallUserDCsecondSeriailzer,UserLoginSeriailzer,UserProfileSerializer,GetallUserSeriailzer,UserModelSerializer,GetallUserWithCompSeriailzer
+from account.serializers import UpdateUserSeriailzer, UserRegistrationSerializer,UserLoginSeriailzer,UserProfileSerializer,GetallUserSeriailzer
 from friend.serializers import FriendShipSerializer
 from django.contrib.auth import authenticate
-from rest_framework.exceptions import AuthenticationFailed
-from django.http import JsonResponse
-from .models import UserManager,User
+from .models import User
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse
 from rest_framework_simplejwt.tokens import RefreshToken
 from account.renderers import UserRenderer
 from rest_framework.permissions import IsAuthenticated
-import jwt, datetime
-from django.conf import settings
-
-# from django_filters.rest_framework import DjangoFilterBackend
-from django.shortcuts import render
-from django.template import Context, RequestContext
-import joblib
-from friend.utils import get_friend_request_or_false
-
-from rest_framework.decorators import action
-import requests
-# from rest_framework import generics
-import json
-from friend.models import FriendList, FriendShip
-import os
-from friend.friend_request_status import FriendRequestStatus
-
+import joblib # from django_filters.rest_framework import DjangoFilterBackend
+from friend.models import FriendShip # from rest_framework import generics
+print('inside views.py')
 # from rest_framework import filters
 model = joblib.load('./ABmodel.joblib')
-
-
 
 #Generate Token Manually
 def get_tokens_for_user(user):
@@ -42,7 +24,6 @@ def get_tokens_for_user(user):
     return {
         'refresh':str(refresh),
         'access': str(refresh.access_token),
-     
     }
 
 class UserRegistrationView(APIView):
@@ -60,36 +41,6 @@ class UserRegistrationView(APIView):
             return Response({'accessToken':accessToken, 'msg':'Registration Successfull'},status=status.HTTP_201_CREATED)
         # print(serializer.errors)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    # @action(detail=True,method=['put'])
-    # def profile(self,request,pk=None):
-    #     user = self.get_object()
-    #     profile = user.profile
-    #     serializer = ProfileSerializer(profile,data=request.data)
-    #     if serializer.is_valid:
-    #         serializer.save()
-    #         return Response(serializer.data,status=200)
-    #     else:
-    #         return Response(serializer.errors,status=400)
-    
-    
-    
-# class UserLoginView(APIView):
-#     renderer_classes = [UserRenderer]
-#     def post(self,request,format=None):
-#         serializer = UserLoginSeriailzer(data=request.data)
-#         if serializer.is_valid(raise_exception=True):
-#             email = serializer.data.get('email')
-#             password = serializer.data.get('password')
-           
-#             user = authenticate(email=email,password=password)
-#             if  user is not None:
-#              token = get_tokens_for_user(user)
-#              return Response({'token':token,'msg':'Login Successfull'},status=status.HTTP_200_OK)
-#             else: 
-#                 return Response({'errors':{'non_field_errors':['Email or Password-- is not Valid']}},status=status.HTTP_404_NOT_FOUND)    
-
-
-
 
 class UserLoginView(APIView):
     renderer_classes = [UserRenderer]
@@ -111,95 +62,16 @@ class UserLoginView(APIView):
              return Response({'name':user.name,'accessToken':accessToken},status=status.HTTP_200_OK)
 
             else: 
-                return Response({'errors':{'non_field_errors':['Email or Password-- is not Valid']}},status=status.HTTP_404_NOT_FOUND)    
-
-
-    
-
-
-# class UserProfileView(APIView):
-#     """
-#     - Logic here is kind of tricky
-#         is_self
-#         is_friend
-#             -1: NO_REQUEST_SENT
-#             0: THEM_SENT_TO_YOU
-#             1: YOU_SENT_TO_THEM
-#     """
-#     def get(self, request, *args, **kwargs):
-#         user_id = kwargs.get("user_id")
-#         try:
-#             account = User.objects.get(pk=user_id)
-#         except User.DoesNotExist:
-#             return Response("Account not found.", status=status.HTTP_404_NOT_FOUND)
-        
-#         account_serializer = UserProfileSerializer(account)
-        
-#         try:
-#             friend_list = FriendList.objects.get(user=account)
-#         except FriendList.DoesNotExist:
-#             friend_list = FriendList(user=account)
-#             friend_list.save()
-        
-#         friends = friend_list.friends.all()
-#         friend_requests = None
-#         user = request.user
-        
-#         # Define template variables
-#         is_self = True
-#         is_friend = False
-#         request_sent = FriendRequestStatus.NO_REQUEST_SENT.value
-        
-#         if user.is_authenticated and user != account:
-#             is_self = False
+                return Response({'errors':{'non_field_errors':['Email or Password-- is not Valid']}},status=status.HTTP_404_NOT_FOUND)
             
-#             if friends.filter(pk=user.id):
-#                 is_friend = True
-#             else:
-#                 is_friend = False
-                
-#                 # CASE1: Request has been sent from THEM to YOU: FriendRequestStatus.THEM_SENT_TO_YOU
-#                 if get_friend_request_or_false(sender=account, receiver=user) != False:
-#                     request_sent = FriendRequestStatus.THEM_SENT_TO_YOU.value
-#                 # CASE2: Request has been sent from YOU to THEM: FriendRequestStatus.YOU_SENT_TO_THEM
-#                 elif get_friend_request_or_false(sender=user, receiver=account) != False:
-#                     request_sent = FriendRequestStatus.YOU_SENT_TO_THEM.value
-#                 # CASE3: No request sent from YOU or THEM: FriendRequestStatus.NO_REQUEST_SENT
-#                 else:
-#                     request_sent = FriendRequestStatus.NO_REQUEST_SENT.value
-        
-#         elif not user.is_authenticated:
-#             is_self = False
-        
-#         else:
-#             try:
-#                 friend_requests = FriendRequest.objects.filter(receiver=user, is_active=True)
-#             except FriendRequest.DoesNotExist:
-#                 pass
-        
-#         account_data = account_serializer.data
-#         friend_requests_serializer = FriendRequestSerializer(friend_requests, many=True)
-        
-#         response_data = {
-#             'account': account_data,
-#             'is_self': is_self,
-#             'is_friend': is_friend,
-#             'request_sent': request_sent,
-#             'friend_requests': friend_requests_serializer.data,
-#             'BASE_URL': settings.BASE_URL
-#         }
-        
-#         return Response(response_data, status=status.HTTP_200_OK)
-
-
-
-# class  UserProfileView(APIView):
-#     renderer_classes = [UserRenderer]
-#     permission_classes = [IsAuthenticated]        
-#     def get(self, request, format=None):
-#         serializer = UserProfileSerializer(request.user)
-#         # json_data = JSONRenderer().render(serializer.data)
-#         return Response(serializer.data,content_type ='application/json')
+class uploadPost(APIView):
+   print('inside upload class')
+   def post(self,request):
+       try:
+        print('hi inside post upload')
+        return Response({'message':'Post upladed'})
+       except:
+        raise APIException('Error uploading the post')
 
 
 class UserProfileView(APIView):
