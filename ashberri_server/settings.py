eca80ae78e1c960f3a46767718bc83c703f950b1
Ashish Thakur <50015357+AshishThakur10@users.noreply.github.com>
2023-08-11 22:24:33 +0530

    db usern name changed

--- ashberri_server/settings.py
+++ ashberri_server/settings.py
@@ -151,7 +151,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ashberridb',
-        'USER': 'ashberri',
+        'USER': 'postgres',
         'PASSWORD': '123',
         'HOST': '127.0.0.1',
         'PORT': '5432',
