f41936e568a082d2674715cde378a84319bdcd08
Aman kumar <amankumar02740@gmail.com>
2025-02-13 21:04:36 +0530

    some rename

--- account/urls.py
+++ account/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('login/', UserLoginView.as_view(),name='login'),
 
     path('profile/', UserProfileView.as_view(), name='profile'),
-    # path('users/', GetAllUser.as_view(),name='getallusers'),# NOTE --this api can be use later on, instead of this api use http://127.0.0.1:8000/account/friendStatusAndrank/
-    # path('users/<int:id>',GetAllUser.as_view(), ),#NOTE --this api can be use later on instead of this api use http://127.0.0.1:8000/account/friendStatusAndrankById/?id=1
+    # path('users/', GetAllUser.as_view(),name='getallusers'),# NOTE --this api can be use later on, instead of this api use http://127.0.0.1:8000/account/AllUserInfoWithFriendStatus/
+    # path('users/<int:id>',GetAllUser.as_view(), ),#NOTE --this api can be use later on instead of this api use http://127.0.0.1:8000/account/UserByIdInfoWithFriendStatus/?id=1
     # path('search/', include('customsearch.urls')),
     path('AllUserInfoWithFriendStatus/', AllUserInfoWithFriendStatus.as_view(), name='AllUserInfoWithFriendStatus'),
     path('UserByIdInfoWithFriendStatus/', UserByIdInfoWithFriendStatus.as_view(), name='UserByIdInfoWithFriendStatus'),
